289ed3b97544c03296e9876772f2ea36e9d1d40e
hydro0o0 <justsimplyhydro@email.com>
2026-01-07 11:52:51 +0700

    Changed Server/TowerStatistics.lua to fix indexing issues.

--- Server/TowerStatistics.lua
+++ Server/TowerStatistics.lua
@@ -1,43 +1,43 @@
 local module = {
 	Pulse = {
 		ExtraInfo = {
-			MaxLevel = 4,
+			MaxLevel = 5,
 			MaxHits = 2,
 			MaxPlacement = 14, 
-			Hidden = {3,4},
+			Hidden = {4,5},
 			Class = {
-				[0] = "Single",
 				[1] = "Single",
 				[2] = "Single",
 				[3] = "Single",
 				[4] = "Single",
+				[5] = "Single",
 			}
 		},
-		[0] = {
+		[1] = {
 			Cooldown = 0.8,
 			Range = 13,
 			Damage = 2,
 			Price = 175,
 		},
-		[1] = {
+		[2] = {
 			Cooldown = 0.8,
 			Range = 15,
 			Damage = 3,
 			Price = 100,
 		},
-		[2] = {
+		[3] = {
 			Cooldown = 0.75,
 			Range = 19,
 			Damage = 6,
 			Price = 400,
 		},
-		[3] = {
+		[4] = {
 			Cooldown = 0.375,
 			Range = 19,
 			Damage = 6,
 			Price = 800,
 		},
-		[4] = {
+		[5] = {
 			Cooldown = 0.155,
 			Range = 21,
 			Damage = 6,
@@ -46,64 +46,64 @@ local module = {
 	},
 	Barrage = {
 		ExtraInfo = {
-			MaxLevel = 5,
+			MaxLevel = 6,
 			Penetration = {
-				[0] = 1.1,
 				[1] = 1.1,
-				[2] = 1.2,
-				[3] = 1.4,
+				[2] = 1.1,
+				[3] = 1.2,
 				[4] = 1.4,
-				[5] = 2,
+				[5] = 1.4,
+				[6] = 2,
 			},
 			SpreadAngle = {
-				[0] = 26,
 				[1] = 26,
-				[2] = 30,
-				[3] = 35,
+				[2] = 26,
+				[3] = 30,
 				[4] = 35,
-				[5] = 40,
+				[5] = 35,
+				[6] = 40,
 			},
-			Hidden = {5},
+			Hidden = {6},
 			Class = {
-				[0] = "Cone",
 				[1] = "Cone",
 				[2] = "Cone",
 				[3] = "Cone",
 				[4] = "Cone",
 				[5] = "Cone",
+				[6] = "Cone",
 			},
 		},
-		[0] = {
+		[1] = {
 			Cooldown = 2,
 			Range = 7,
 			Damage = 4,
 			Price = 500,
 		},
-		[1] = {
+		[2] = {
 			Cooldown = 1.6,
 			Range = 7,
 			Damage = 4,
 			Price = 300,
 		},
-		[2] = {
+		[3] = {
 			Cooldown = 1.5,
 			Range = 7.5,
 			Damage = 6,
 			Price = 500,
 		},
-		[3] = {
+		[4] = {
 			Cooldown = 1.4,
 			Range = 8,
 			Damage = 10,
 			Price = 650,
 		},
-		[4] = {
+		[5] = {
 			Cooldown = 0.9,
 			Range = 8,
 			Damage = 22,
 			Price = 1900,
 		},
-		[5] = {
+		[6] = {
 			Cooldown = 0.8,
 			Range = 9,
 			Damage = 25,
@@ -113,43 +113,43 @@ local module = {
 
 	Vesper = {
 		ExtraInfo = {
-			MaxLevel = 4,
+			MaxLevel = 5,
 			HighGround = true,
-			Air = {0,1,2,3,4},
-			Hidden = {0,1,2,3,4},
+			Air = {1,2,3,4,5},
+			Hidden = {1,2,3,4,5},
 			Class = {
-				[0] = "Single",
 				[1] = "Single",
 				[2] = "Single",
 				[3] = "Single",
 				[4] = "Single",
+				[5] = "Single",
 			},
 		},
-		[0] = {
+		[1] = {
 			Cooldown = 3.75,
 			Range = 26,
 			Damage = 15,
 			Price = 375,
 		},
-		[1] = {
+		[2] = {
 			Cooldown = 3.6,
 			Range = 28,
 			Damage = 28,
 			Price = 500,
 		},
-		[2] = {
+		[3] = {
 			Cooldown = 3.45,
 			Range = 33,
 			Damage = 45,
 			Price = 975,
 		},
-		[3] = {
+		[4] = {
 			Cooldown = 3.2,
 			Range = 39,
 			Damage = 90,
 			Price = 2000,
 		},
-		[4] = {
+		[5] = {
 			Cooldown = 2.8,
 			Range = 41,
 			Damage = 195,
@@ -159,41 +159,41 @@ local module = {
 
 	Moonfall = {
 		ExtraInfo = {
-			MaxLevel = 4,
-			Hidden = {2,3,4},
+			MaxLevel = 5,
 			Class = {
-				[0] = "Single",
 				[1] = "Single",
 				[2] = "Single",
 				[3] = "Single",
 				[4] = "Single",
+				[5] = "Single",
 			},
+			Hidden = { 3, 4, 5 },
 		},
-		[0] = {
+		[1] = {
 			Cooldown = 1.4,
 			Range = 5,
 			Damage = 8,
 			Price = 400,
 		},
-		[1] = {
+		[2] = {
 			Cooldown = 1.2,
 			Range = 5.5,
 			Damage = 10,
 			Price = 525,
 		},
-		[2] = {
+		[3] = {
 			Cooldown = 1,
 			Range = 6,
 			Damage = 20,
 			Price = 1050,
 		},
-		[3] = {
+		[4] = {
 			Cooldown = 0.9,
 			Range = 6.5,
 			Damage = 40,
 			Price = 3500,
 		},
-		[4] = {
+		[5] = {
 			Cooldown = 0.8,
 			Range = 8,
 			Damage = 55,
@@ -203,42 +203,42 @@ local module = {
 
 	Exterminator = {
 		ExtraInfo = {
-			MaxLevel = 4,
+			MaxLevel = 5,
 			MaxPlacement = 7,
-			Hidden = {2,3,4},
+			Hidden = { 3, 4, 5 },
 			Class = {
-				[0] = "Single",
 				[1] = "Single",
 				[2] = "Single",
 				[3] = "Single",
 				[4] = "Single",
+				[5] = "Single",
 			},
 		},
-		[0] = {
+		[1] = {
 			Cooldown = 0.48,
 			Range = 15,
 			Damage = 12,
 			Price = 3500,
 		},
-		[1] = {
+		[2] = {
 			Cooldown = 0.48,
 			Range = 16,
 			Damage = 18,
 			Price = 1500,
 		},
-		[2] = {
+		[3] = {
 			Cooldown = 0.48,
 			Range = 16,
 			Damage = 35,
 			Price = 3500,
 		},
-		[3] = {
+		[4] = {
 			Cooldown = 0.358,
 			Range = 18,
 			Damage = 45,
 			Price = 6500,
 		},
-		[4] = {
+		[5] = {
 			Cooldown = 0.22,
 			Range = 22,
 			Damage = 50,
@@ -248,62 +248,62 @@ local module = {
 
 	Blitzkrieg = {
 		ExtraInfo = {
-			MaxLevel = 4,
+			MaxLevel = 5,
 			AOE = {
-				[0] = {
+				[1] = {
 					Studs = 3,
 					Delayed = 0.5 --if delayed is undefined it will be 0, same goes for stud except its default to 3
 				},
-				[1] = {
+				[2] = {
 					Studs = 4,
 					Delayed = 0.5
 				},
-				[2] = {
+				[3] = {
 					Studs = 6,
 					Delayed = 0.4
 				},
-				[3] = {
+				[4] = {
 					Studs = 6,
 					Delayed = 0.3
 				},
-				[4] = {
+				[5] = {
 					Studs = 7,
 					Delayed = 0.25
 				},
 			},
 			Class = {
-				[0] = "AOE",
 				[1] = "AOE",
 				[2] = "AOE",
 				[3] = "AOE",
 				[4] = "AOE",
+				[5] = "AOE",
 			},
 		},
-		[0] = {
+		[1] = {
 			Cooldown = 3,
 			Range = 7,
 			Damage = 8,
 			Price = 500,
 		},
-		[1] = {
+		[2] = {
 			Cooldown = 2.7,
 			Range = 8,
 			Damage = 8,
 			Price = 650,
 		},
-		[2] = {
+		[3] = {
 			Cooldown = 2.3,
 			Range = 9,
 			Damage = 14,
 			Price = 2500,
 		},
-		[3] = {
+		[4] = {
 			Cooldown = 2.3,
 			Range = 13,
 			Damage = 14,
 			Price = 3000,
 		},
-		[4] = {
+		[5] = {
 			Cooldown = 2,
 			Range = 20,
 			Damage = 34,
@@ -313,44 +313,44 @@ local module = {
 
 	Skylight = {
 		ExtraInfo = {
-			MaxLevel = 4,
+			MaxLevel = 5,
 			HighGround = true,
 			MaxPlacement = 8,
-			Air = {0,1,2,3,4},
-			Hidden = {4},
+		Air = { 1, 2, 3, 4 , 5},
+			Hidden = { 5 },
 			Class = {
-				[0] = "Single",
 				[1] = "Single",
 				[2] = "Single",
 				[3] = "Single",
 				[4] = "Single",
+				[5] = "Single",
 			},
 		},
-		[0] = {
+		[1] = {
 			Cooldown = 6,
 			Range = 50,
 			Damage = 200,
 			Price = 5000,
 		},
-		[1] = {
+		[2] = {
 			Cooldown = 5,
 			Range = 50,
 			Damage = 200,
 			Price = 1350,
 		},
-		[2] = {
+		[3] = {
 			Cooldown = 4,
 			Range = 50,
 			Damage = 250,
 			Price = 4150,
 		},
-		[3] = {
+		[4] = {
 			Cooldown = 8,
 			Range = 75,
 			Damage = 1000,
 			Price = 10000,
 		},
-		[4] = {
+		[5] = {
 			Cooldown = 16,
 			Range = 75,
 			Damage = 4000,
@@ -360,63 +360,63 @@ local module = {
 	
 	Anomaly = {
 		ExtraInfo = {
-			Hidden = {0},
-			MaxLevel = 4,
+			Hidden = {1,2,3,4,5},
+			MaxLevel = 5,
 			AOE = {
-				[0] = {
+				[1] = {
 					Studs = 3,
 					Delayed = 0.5 --if delayed is undefined it will be 0, same goes for stud except its default to 3
 				},
-				[1] = {
+				[2] = {
 					Studs = 4,
 					Delayed = 0.5
 				},
-				[2] = {
+				[3] = {
 					Studs = 6,
 					Delayed = 0.4
 				},
-				[3] = {
+				[4] = {
 					Studs = 6,
 					Delayed = 0.3
 				},
-				[4] = {
+				[5] = {
 					Studs = 7,
 					Delayed = 0.25
 				},
 			},
 			Class = {
-				[0] = "AOE",
 				[1] = "AOE",
 				[2] = "AOE",
 				[3] = "AOE",
 				[4] = "AOE",
+				[5] = "AOE",
 			},
 		},
-		[0] = {
+		[1] = {
 			Cooldown = 1,
 			Range = 3,
 			Damage = 5,
 			Price = 500,
 		},
-		[1] = {
+		[2] = {
 			Cooldown = 1,
 			Range = 3,
 			Damage = 8,
 			Price = 200,
 		},
-		[2] = {
+		[3] = {
 			Cooldown = 0.75,
 			Range = 3,
 			Damage = 11,
 			Price = 1000,
 		},
-		[3] = {
+		[4] = {
 			Cooldown = 0.5,
 			Range = 4,
 			Damage = 17,
 			Price = 1850,
 		},
-		[4] = {
+		[5] = {
 			Cooldown = 0.3,
 			Range = 4,
 			Damage = 30,
